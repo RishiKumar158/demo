18a5ead83615da280f54a259de7ecff2bde48003
Rishi Kumar <sarakana.rishi@gmail.com>
2025-09-23 10:01:00 +0530

    initial setup

---
+++ src/test/java/com/sarakana/demo/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sarakana.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
